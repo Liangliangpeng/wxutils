b4a57e346023d4e363d43e41d238384476625c7b
li-yin-long <18312884980@163.com>
2019-05-09 11:17:40 +0800

    完成openid解密、公众号校验token接口。

--- src/main/java/com/longdatech/decryptcode/utils/WxConstant.java
+++ src/main/java/com/longdatech/decryptcode/utils/WxConstant.java
@@ -7,8 +7,8 @@ package com.longdatech.decryptcode.utils;
  */
 public class WxConstant {
 
-    private static final String appid = "wx2f1ee596ff9062b7";//小程序appid
-    private static final String secret = "9e25135c13668e106aac50064c09892e";//小程序密钥
+    private static final String appid = "";//小程序appid
+    private static final String secret = "";//小程序密钥
     private static final String grant_type = "authorization_code";//授权类型，此处只需填写 authorization_code
 
     //参考：https://developers.weixin.qq.com/miniprogram/dev/api-backend/auth.code2Session.html
